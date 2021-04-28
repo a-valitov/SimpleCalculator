f089847ce2af76de3b5200883759b9b1ffe0c007
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-04-28 15:57:04 +0500

    Digits and dots are working

--- app/src/main/java/com/avalitov/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/avalitov/simplecalculator/MainActivity.kt
@@ -3,15 +3,41 @@ package com.avalitov.simplecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvInput: TextView
+
+    var lastNumeric : Boolean = false   // the last inputted character is a number
+    var lastDot: Boolean = false        // the last inputted character is a dot
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
+        tvInput.append((view as Button).text)
+        lastNumeric = true
+    }
+
+    fun onClear(view: View) {
+        tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View) {
+        if(lastNumeric && !lastDot) {
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
