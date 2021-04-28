1adb7565dc5e0cdf0898ab226e45f625ad3612e7
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-04-28 20:44:50 +0500

    Subtraction realized

--- app/src/main/java/com/avalitov/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/avalitov/simplecalculator/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,9 +23,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDigit(view: View) {
-//        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
         tvInput.append((view as Button).text)
         lastNumeric = true
+
     }
 
     fun onClear(view: View) {
@@ -40,4 +41,54 @@ class MainActivity : AppCompatActivity() {
             lastDot = true
         }
     }
+
+    fun onEqual(view: View) {
+        if(lastNumeric) {
+            var tvValue = tvInput.text.toString()
+            var prefix = ""
+
+            try {
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)  // throw away the 1st character in the string
+                }
+
+                if (tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+//                    var one = splitValue[splitValue.size - 2]
+//                    var two = splitValue[splitValue.size - 1]
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                }
+
+
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun onOperator(view: View) {
+        if (lastNumeric && !isOperatorAdded(tvInput.text.toString())) {
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/") || value.contains("*")
+                    || value.contains("+") || value.contains("-")
+        }
+    }
+
 }
